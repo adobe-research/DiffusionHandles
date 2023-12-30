23a8e3b6e089488643c0987755e83dd199a8a964
Paul Guerrero <guerrero@adobe.com>
2023-12-30 15:38:43 +0000

    updated test script

--- test/test.py
+++ test/test.py
@@ -5,21 +5,24 @@ from diffhandles import ImageEditor
 
 
 def test():
-    rot_angle = 55.0
+    # sunflower stop-motion rotation
+    rot_angles = [30.0, 55.0, 60.0]
     rot_axis = torch.tensor([0.0, 1.0, 0.0])
     translation = (0.0, 0.0, 0.0)
     input_img_path = 'data/sunflower.png'
-    edited_img_path = 'results/sunflower_edited.png'
+    edited_img_path_template = 'results/sunflower'
     prompt = "a sunflower in the garden"
     fg_phrase = "sunflower"
     bg_phrase = "garden"
     
     input_img = load_image(input_img_path).unsqueeze(dim=0)
-    img_editor = ImageEditor()
-    edited_img = img_editor.edit_image(
-        img=input_img, prompt=prompt, fg_phrase=fg_phrase, bg_phrase=bg_phrase,
-        rot_angle=rot_angle, rot_axis=rot_axis, translation=translation)
-    save_image(edited_img.squeeze(dim=0), edited_img_path)
+    for rot_angle in rot_angles:
+        img_editor = ImageEditor()
+        edited_img = img_editor.edit_image(
+            img=input_img, prompt=prompt, fg_phrase=fg_phrase, bg_phrase=bg_phrase,
+            rot_angle=rot_angle, rot_axis=rot_axis, translation=translation)
+        edited_img_path = f'{edited_img_path_template}_rotated_{rot_angle:.0f}.png'
+        save_image(edited_img.squeeze(dim=0), edited_img_path)
 
 def load_image(path: str) -> torch.Tensor:
     img = Image.open(path)
